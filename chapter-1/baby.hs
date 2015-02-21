e55d39c8325ee5100a375d5026e0a04a9bbe7d90
Greg Symons <gsymons@drillinginfo.com>
2015-02-21 14:45:30 -0600

    Baby's first steps...

---
+++ chapter-1/baby.hs
@@ -0,0 +1,13 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x * 2
+
+length' xs = sum [ 1 | _ <- xs ]
+
+removeNonUpperCase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+
